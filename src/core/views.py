f0ac06ce191f6e73105c715cc20a8fbe3469518e
abeer <abeer.sewak@gmail.com>
2020-02-07 16:03:03 -0600

    Load expenses in detail view

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Expense, Category
 
 
 def project_list(request):
@@ -8,4 +8,8 @@ def project_list(request):
 
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
-    return render(request, "project-detail.html", {"project": project})
+    return render(
+        request,
+        "project-detail.html",
+        {"project": project, "expense_list": project.expenses.all()},
+    )
