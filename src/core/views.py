2f7f781446215ca2df09818594d12ac1be989adc
abeer <abeer.sewak@gmail.com>
2020-02-07 15:40:20 -0600

    updated views logic to route to detail page

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 
 def project_list(request):
@@ -6,4 +7,5 @@ def project_list(request):
 
 
 def project_detail(request, project_slug):
-    return render(request, "project-detail.html")
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, "project-detail.html", {"project": project})
