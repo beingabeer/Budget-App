3a4566a15659d180888be9bafdf04ba329d95778
abeer <abeer.sewak@gmail.com>
2020-02-07 15:08:24 -0600

    add str representation for all models

--- src/core/models.py
+++ src/core/models.py
@@ -20,12 +20,18 @@ class Category(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Expense(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 def create_slug(instance, new_slug=None):
