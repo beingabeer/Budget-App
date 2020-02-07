97da33c56afbc24e538a1b8c2c3743276aa5a70a
abeer <abeer.sewak@gmail.com>
2020-02-07 14:40:10 -0600

    Added the Project models

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    budget = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Project, self).save(*args, **kwargs)
+
+
+class Expense(models.Model):
+    pass
